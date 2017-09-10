a998bb635486b0b692f6b03ded88dc762d0bd1d2
Omkar Tondawalkar <tondawalkar.omkar@gmail.com>
2017-09-10 15:35:32 +0530

    React js Hello World

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+/*var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  }
+};
+
+module.exports = config;
+
+*/
+
+var config = {
+   entry: './main.js',
+	
+   output: {
+      path:'/',
+      filename: 'index.js',
+   },
+	
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+	
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+				
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+
+}
+
+module.exports = config;
